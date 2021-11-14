2e608599ca226bb9517b7ed7cc5b9d4eea1916e1
David <81191281+droblee@users.noreply.github.com>
2021-11-14 07:45:25 -0800

    find user input

--- scriptPull.js
+++ scriptPull.js
@@ -57,11 +57,14 @@ async function funcClickFindUser(){
     
         var htmlTag_hr = document.createElement("hr");
         var htmlTag_h2 = document.createElement("h2");
+        var htmlTag_input = document.createElement("input");
     
         htmlTag_h2.innerHTML = "Find User"
+        htmlTag_input.id = "findUserInput"
     
         document.getElementById("divAdditionalOptions").appendChild(htmlTag_h2);
         document.getElementById("divAdditionalOptions").appendChild(htmlTag_hr);
+        document.getElementById("divAdditionalOptions").appendChild(htmlTag_input);
 
         bolFindUserButtonClicked = true;
     }    
